bc873413199a2c4153bd5bcf5a33468a3e9bcc27
CiaranCurran <ciaran.curran@ucdconnect.ie>
2017-04-17 17:36:40 +0000

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "operations.h"
+
+
+int main(void){
+	const char *playerTypes[]= {"Elf", "Human", "Ogre", "Wizard"};
+	const char *slotTypes[]= {"Level Ground", "Hill", "City"};
+	
+	int boardSize, numPlayers, i;
+	
+	struct player players[MAX_PLAYERS];//can take maximum size of players
+
+	boardSize = BOARD_SIZE;
+	
+	slot ** board= malloc(boardSize * sizeof(slot *)); //initialise board 
+  
+  
+}
